5622b07e927153c6621366022a8f0f49ad0b94c1
Your Name <diegod9405@gmail.com>
2023-07-10 18:09:51 -0600

    Fin de seccion 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Mi primera app en angular';
+  public counter: number  = 10;
+
+  increaseBy(value:number):void {
+    this.counter = this.counter +1;
+  }
+  decreaseBy(value:number):void{
+    this.counter = this.counter -value;
+  }
+  reset():void{
+    this.counter = 10; 
+  }
+
 }
